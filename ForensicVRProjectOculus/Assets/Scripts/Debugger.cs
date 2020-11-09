fd270b794cd4176028f2700435393779c11a1db1
NatPlatt <n.zollinger26@hotmail.com>
2020-11-09 13:52:41 -0700

    testing object pick up

---
+++ ForensicVRProjectOculus/Assets/Scripts/Debugger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugger : MonoBehaviour
+{
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("I am triggered");
+    }
+}
