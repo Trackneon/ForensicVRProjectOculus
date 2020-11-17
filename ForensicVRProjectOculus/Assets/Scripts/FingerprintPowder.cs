60c1d5b1837b9d48c451c8f9888c6f0d763d4d62
NatPlatt <n.zollinger26@hotmail.com>
2020-11-16 17:53:28 -0700

    testing and adding fingerprinting functionality

---
+++ ForensicVRProjectOculus/Assets/Scripts/FingerprintPowder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerprintPowder : MonoBehaviour
+{
+    public Material material;
+    public List<Material> materialsList;
+    public int triggerCount = 0;
+    
+    public void AddPowder()
+    {
+        triggerCount += 1;
+        material = materialsList[triggerCount];
+    }
+}
