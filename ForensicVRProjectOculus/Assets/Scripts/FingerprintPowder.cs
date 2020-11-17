3783f9ac4cd6f04bc88664ad443ffdc0929add85
NatPlatt <n.zollinger26@hotmail.com>
2020-11-16 19:38:27 -0700

    adding to fingerprinting functionality script

--- ForensicVRProjectOculus/Assets/Scripts/FingerprintPowder.cs
+++ ForensicVRProjectOculus/Assets/Scripts/FingerprintPowder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,22 @@ public class FingerprintPowder : MonoBehaviour
     public Material material;
     public List<Material> materialsList;
     public int triggerCount = 0;
-    
+    public GameObject fingerprint;
+    public GameObject powderBrush;
+
+    public void Start()
+    {
+        material = fingerprint.GetComponent<Renderer>().material;
+    }
+
     public void AddPowder()
     {
         triggerCount += 1;
         material = materialsList[triggerCount];
+        if (powderBrush.gameObject.tag == "powderBrush")
+        {
+            fingerprint.GetComponent<Renderer>().material = material;
+        }
+        
     }
 }
