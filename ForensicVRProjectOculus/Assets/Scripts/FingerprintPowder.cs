73b18d07fa1256662b276537cf10c35e35f34bb7
NatPlatt <n.zollinger26@hotmail.com>
2020-11-18 18:50:14 -0700

    refining fingerprinting functionality

--- ForensicVRProjectOculus/Assets/Scripts/FingerprintPowder.cs
+++ ForensicVRProjectOculus/Assets/Scripts/FingerprintPowder.cs
@@ -14,15 +14,22 @@ public class FingerprintPowder : MonoBehaviour
        colorRender = fingerprint.GetComponent<Renderer>();
     }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        AddPowder();
+    }
+
     public void AddPowder()
     {
         if (powderBrush.gameObject.tag == "powderBrush")
         {
-            for (float touchNum = 1f; touchNum >= 0; touchNum += 0.1f)
+            for (float touchNum = 0.1f; touchNum <= 1; touchNum += 0.1f)
             {
                 Color powderColor = colorRender.material.color;
                 powderColor.a = touchNum;
                 colorRender.material.color = powderColor;
+                Debug.Log("touch num is "+touchNum);
+                
             }
         }
         
