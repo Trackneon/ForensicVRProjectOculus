14fd1c3f15f0b8dea47817595ef6338b72050d61
NatPlatt <n.zollinger26@hotmail.com>
2020-11-23 15:35:44 -0700

    adding paintable functionality to fingerprinting

---
+++ ForensicVRProjectOculus/Assets/Scripts/Paintable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Paintable : MonoBehaviour
+{
+    public GameObject powder;
+
+    public void PaintWithPowder()
+    {
+        var go = Instantiate(powder, Vector3.up * 0.1f, Quaternion.identity, transform);
+    }
+}
