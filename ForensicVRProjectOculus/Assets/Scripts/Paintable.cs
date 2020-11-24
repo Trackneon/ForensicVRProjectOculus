17d52410663861b9142e42ffc493fb7ac7b49f0f
NatPlatt <n.zollinger26@hotmail.com>
2020-11-23 18:39:03 -0700

    added to fingerprinting script

--- ForensicVRProjectOculus/Assets/Scripts/Paintable.cs
+++ ForensicVRProjectOculus/Assets/Scripts/Paintable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -6,10 +7,17 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Paintable : MonoBehaviour
 {
     public GameObject powder;
+    public Vector3 tipPoint;
+    public Transform transform;
+
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
 
     public void PaintWithPowder()
     {
-        Instantiate(powder, Vector3.up * 0.1f, Quaternion.identity, transform);
+        Instantiate(powder, transform.localPosition, Quaternion.identity, transform);
         Debug.Log("powdering");
     }
 }
