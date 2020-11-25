d1337d7d867c8c0c2bcc115737071f7558c567ee
NatPlatt <n.zollinger26@hotmail.com>
2020-11-24 18:23:04 -0700

    added tweezer and ALS functionality

---
+++ ForensicVRProjectOculus/Assets/ALSColorSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ALSColorSwitch : MonoBehaviour
+{
+
+    public List<Light> lightColors;
+    public Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+        myLight.enabled = false;
+    }
+
+    public void ChangeLight()
+    {
+        int i = 0;
+        foreach (var light in lightColors)
+        {
+            myLight.enabled = false;
+            i++;
+            myLight = lightColors[i];
+            myLight.enabled = true;
+        }
+
+        
+
+        if (i >= lightColors.Count)
+        {
+            i = 0;
+        }
+    }
+}
