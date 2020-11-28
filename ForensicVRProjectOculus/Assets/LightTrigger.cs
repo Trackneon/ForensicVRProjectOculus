2d47b9e63878ca13bba8735d62256373df78d84e
NatPlatt <n.zollinger26@hotmail.com>
2020-11-28 16:53:34 -0700

    adding to ALS, writing scripts for matching system

---
+++ ForensicVRProjectOculus/Assets/LightTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : ALSColorSwitch
+{
+    public GameObject visibleByALS;
+    public string colorActivate;
+    void Start()
+    {
+        visibleByALS.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //if (visibleByALS.gameObject.tag == "ALS" && colorActivate == "UV" && lightColors[2]==isActiveAndEnabled)
+        {
+            visibleByALS.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        visibleByALS.SetActive(false);
+    }
+}
---
+++ ForensicVRProjectOculus/Assets/Scripts/MatchingSystem/MatchIDBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class MatchIDBehavior : IDBehavior
+{
+    [Serializable]
+    public struct PossibleWork
+    {
+        public NameID nameIdObj;
+        public UnityEvent EnterEvent, ExitEvent;
+    }
+
+    private NameID otherIdObj;
+    private IDBehavior otherBehaviorObj;
+    public List<PossibleWork> workIdList;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        otherBehaviorObj = other.GetComponent<IDBehavior>();
+        if (otherBehaviorObj == null) return;
+
+        otherIdObj = otherBehaviorObj.nameIdObj;
+        CheckID(1);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        otherBehaviorObj = other.GetComponent<IDBehavior>();
+        if (otherBehaviorObj == null) return;
+        otherIdObj = otherBehaviorObj.nameIdObj;
+        CheckID(2);
+    }
+
+    private void CheckID(int stateNumber)
+    {
+        foreach (var obj in workIdList)
+        {
+            if (otherIdObj == obj.nameIdObj)
+            {
+                switch (stateNumber)
+                {
+                    case 1:
+                        obj.EnterEvent.Invoke();
+                        break;
+                    case 2:
+                        obj.ExitEvent.Invoke();
+                        break;
+                }
+            }
+        }
+    }
+}
+
